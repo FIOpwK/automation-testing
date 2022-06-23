8c2dfa05781f657f66a146ee1cc8d81db92279a3
fiopwk <flopwiki@gmail.com>
2022-06-22 20:40:50 -0400

    added Attributes file

--- app/src/main/java/automation/testing/App.java
+++ app/src/main/java/automation/testing/App.java
@@ -5,7 +5,7 @@ package automation.testing;
 
 public class App {
     public String getGreeting() {
-        return "Hello World!";
+        return "Java Rocks !";
     }
 
     public static void main(String[] args) {
---
+++ app/src/test/java/automation/testing/Attributes.java
@@ -0,0 +1,23 @@
+package automation.testing;
+
+public class Attributes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static String Person(String name, int age, float height, double weight, String eyeColor, String gender) {
+		name = "Eclipes";
+		age = 20;
+		height = 4.0f;
+		weight = 23.8d;
+		eyeColor = "Brown";
+		gender = "Male";
+		System.out.println(name);
+		
+		return name;
+		
+	}
+
+}
